56bcd3be0006d2538ceafad80e4c36e27ae5eec0
Alexandre-Mir <110476249+Alexandre-Mir@users.noreply.github.com>
2025-12-29 00:48:28 -0300

    feat: define basic investigator meta types

---
+++ src/types/Investigator.ts
@@ -0,0 +1,16 @@
+export enum Provenance {
+  Imported = 'imported',
+  Created = 'created',
+  Duplicated = 'duplicated'
+}
+
+export type Ruleset = 'classic' | 'pulp'
+
+export interface IInvestigatorMeta {
+  readonly id: string;
+  readonly createdAt: string;
+  updatedAt: string;
+  schemaVersion: number;
+  provenance: Provenance;
+  ruleset: Ruleset;
+}
